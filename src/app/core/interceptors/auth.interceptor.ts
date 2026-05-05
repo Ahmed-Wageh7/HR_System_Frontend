436aaa14ed0e40b7cb25bc2d75fa1ff42c31d71b
A7med-Wageh <gimme.your.number.770@gmail.com>
2026-05-05 22:36:36 +0300

    fix varcel deployment

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,6 +3,16 @@ import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  if (
+    req.url.includes('/auth/login') ||
+    req.url.includes('/auth/refresh-token') ||
+    req.url.includes('/auth/logout') ||
+    req.url.includes('/auth/forgot-password') ||
+    req.url.includes('/auth/reset-password')
+  ) {
+    return next(req);
+  }
+
   const token = inject(AuthService).getAccessToken();
   const authReq = token
     ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
